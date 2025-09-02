2b7d949f3682581b0aeb9b6eb9b9116437200868
akshatparate03 <akshatparate@gmail.com>
2025-09-02 22:58:04 +0530

    Updated responsiveness and design changes

--- src/components/home/Video.jsx
+++ src/components/home/Video.jsx
@@ -1,6 +1,6 @@
 const Video = () => {
   return (
-    <div className="lg:h-full h-205 w-full">
+    <div className="h-full w-full">
       <video
         className="h-full w-full object-cover"
         autoPlay
