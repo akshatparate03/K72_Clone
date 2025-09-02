1423078911d6c74f6dc1698db3f464db98b1019e
akshatparate03 <akshatparate@gmail.com>
2025-09-02 22:44:16 +0530

    Updated responsiveness and design changes

--- src/components/home/HomeHeroText.jsx
+++ src/components/home/HomeHeroText.jsx
@@ -2,13 +2,13 @@ import Video from "./Video";
 
 const HomeHeroText = () => {
   return (
-    <div className="font-[font1]  mt-82 lg:mt-0 pt-5 text-center">
+    <div className="font-[font1]  mt-70 lg:mt-0 pt-5 text-center">
       <div className="lg:text-[9.5vw] text-[14vw] justify-center flex items-center uppercase lg:leading-[8vw] leading-[12vw]">
         L'étincelle
       </div>
       <div className="lg:text-[9.5vw] text-[14vw] justify-center flex items-start uppercase lg:leading-[8vw] leading-[12vw]">
         qui
-        <div className="h-[7vw] w-[16vw] rounded-full overflow-hidden ">
+        <div className="h-[9vw] w-[16vw] rounded-full overflow-hidden ">
           <Video />
         </div>
         génère
